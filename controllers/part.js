3f0307143014eaef7f9bda6d08a08d7df5eaedda
saishanmukh591 <89558232+saishanmukh591@users.noreply.github.com>
2021-11-17 17:04:49 -0600

    Created bonus

--- controllers/part.js
+++ controllers/part.js
@@ -11,23 +11,10 @@ exports.part_list = async function(req, res) {
     } 
    };
 
-// for a specific Part.
-exports.part_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: part detail: ' + req.params.id);
-};
-// Handle part create on POST.
-// exports.part_create_post = function(req, res) {
-
-//  res.send('NOT IMPLEMENTED: part create POST');
-// };
 // Handle part create on POST.
 exports.part_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Part();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"part_type":"goat", "cost":12, "size":"large"}
     document.part_name = req.body.part_name;
     document.cost = req.body.cost;
     document.size = req.body.size;
@@ -41,16 +28,36 @@ exports.part_create_post = async function(req, res) {
     } 
    };
 
-
-
-
-// Handle part delete form on DELETE.
-exports.part_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: part delete DELETE ' + req.params.id);
-};
+   // Handle Part delete on DELETE.
+   exports.part_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Part.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 // Handle part update form on PUT.
-exports.part_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: part update PUT' + req.params.id);
+exports.part_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+     try {
+     let toUpdate = await Part.findById( req.params.id)
+     // Do updates of properties
+     console.log(req.params.id)
+     if(req.body.part_name)  toUpdate.part_name = req.body.part_name;
+     if(req.body.cost) toUpdate.cost = req.body.cost;
+     if(req.body.size) toUpdate.size = req.body.size;
+     let result = await toUpdate.save();
+     console.log("Sucess " + result)
+     res.send(result)
+     } catch (err) {
+     res.status(500)
+     res.send(`{"error": ${err}: Update for id ${req.params.id} 
+    failed`);
+     }
 };
 
 // VIEWS
@@ -65,3 +72,72 @@ exports.part_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     } 
    };
+
+// for a specific Part.
+exports.part_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Part.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+   // Handle a show one view with id specified by query
+exports.part_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Part.findById(req.query.id)
+        res.render('partdetail',
+            { title: 'Part Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+    }
+};
+
+// Handle building the view for creating a part.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.part_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('partcreate', { title: 'Part Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a part.
+// query provides the id
+exports.part_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Part.findById(req.query.id)
+        res.render('partupdate', { title: 'Part Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.part_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Part.findById(req.query.id)
+        res.render('partdelete', {
+            title: 'Part Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sai Shanmukh Garugu' });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Part Login', user: req.user });
+});
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  if (req.session.returnTo)
+      res.redirect(req.session.returnTo);
+  res.redirect('/');
+});
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
