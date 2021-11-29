840623a7b7cede2ceeb018cdb3deb7f17e152ca6
saishanmukh591 <89558232+saishanmukh591@users.noreply.github.com>
2021-11-28 19:46:03 -0600

    acoount js added

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var Part = require("./models/part");
 
 var indexRouter = require('./routes/index');
@@ -25,14 +23,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(require('express-session')({
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+      Account.findOne({ username: username }, function(err, user) {
+          if (err) { return done(err); }
+          if (!user) {
+              return done(null, false, { message: 'Incorrect username.' });
+          }
+          if (!user.validPassword(password)) {
+              return done(null, false, { message: 'Incorrect password.' });
+          }
+          return done(null, user);
+      });
+  }));
+
+
+  app.use(require('express-session')({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false
 }));
+
+
+
 app.use(passport.initialize());
 app.use(passport.session());
+// passport config
+// Use the existing connection
+// The Account model
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
